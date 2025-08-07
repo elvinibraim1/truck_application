1cbc2c049b0fd7f3e231b38f878f39315fe3813a
Ibraim Elvin <ibraimelvin@gmail.com>
2025-08-07 13:10:34 +0300

    Render

--- ocr_excel_app/render-build.sh
+++ ocr_excel_app/render-build.sh
@@ -6,4 +6,6 @@ set -e
 apt-get update && apt-get install -y tesseract-ocr
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r ocr_excel_app/requirements.txt
+
+python -c "import pytesseract; pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'"
